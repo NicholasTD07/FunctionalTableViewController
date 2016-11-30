e2fbb475bbeec8dbb09cfce1e8c394735f39d4c2
NicholasTD07 <Nicholas.TD07@gmail.com>
2016-11-30 12:32:46 +1100

    xcode.create(.project)

---
+++ FunctionalTableViewController/FunctionalTableViewController.h
@@ -0,0 +1,19 @@
+//
+//  FunctionalTableViewController.h
+//  FunctionalTableViewController
+//
+//  Created by Nicholas Tian on 30/11/2016.
+//  Copyright © 2016 nicktd. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for FunctionalTableViewController.
+FOUNDATION_EXPORT double FunctionalTableViewControllerVersionNumber;
+
+//! Project version string for FunctionalTableViewController.
+FOUNDATION_EXPORT const unsigned char FunctionalTableViewControllerVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <FunctionalTableViewController/PublicHeader.h>
+
+
